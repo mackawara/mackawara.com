602a761a465c762d576950958270f9ff6b450b55
mackawara <84657473+mackawara@users.noreply.github.com>
2022-06-26 20:58:22 +0200

    feat:added port 5000 for devlopmetn testing

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
+const multer = require("multer");
+const { dirname } = require("path");
+
+// server config
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("server listening");
+});
+app.get("/", (req, res, next) => {
+  res.sendFile(dirname + "/public/index.html");
+});
+app.use(express.static("public"));
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
