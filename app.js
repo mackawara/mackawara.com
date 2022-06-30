899d8e48f4fa26d645fb0f2c294a9bf9703c9a63
mackawara <84657473+mackawara@users.noreply.github.com>
2022-06-30 17:19:27 +0200

    added express server

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
-const multer = require("multer");
+
 const { dirname } = require("path");
 
 // server config
@@ -12,7 +12,7 @@ app.listen(PORT, () => {
   console.log("server listening");
 });
 app.get("/", (req, res, next) => {
-  res.sendFile(dirname + "/public/index.html");
+  res.sendFile(__dirname + "/public/index.html");
 });
 app.use(express.static("public"));
 app.use(express.json());
