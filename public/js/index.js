352ac02e7e9cda3213c333ccb504423008f7456a
mackawara <84657473+mackawara@users.noreply.github.com>
2022-07-01 14:09:53 +0200

    added toggle button for chat form

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,16 @@
 window.addEventListener("DOMContentLoaded", async () => {
+  const chatbotFormWrapper = document.getElementById("chatbotForm");
   document.getElementById("chatbot").addEventListener("click", () => {
-    const chatbotForm = document.getElementById("chatbotForm");
-    chatbotForm.style.display = "flex";
+    chatbotFormWrapper.style.display = "flex";
+  });
+  document.getElementById("closeChat").addEventListener("click", () => {
+    chatbotFormWrapper.style.display = "none";
+  });
+
+  const chatForm = document.getElementById("chatForm");
+  chatForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const message = chatForm.querySelector("textarea").value;
+    console.log(message);
   });
 });
