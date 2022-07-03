8ae63192be4ff7ef47a65112d99ba763d75a3463
mackawara <84657473+mackawara@users.noreply.github.com>
2022-07-03 19:57:42 +0200

    added wit ai server side handling

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 //const multer = require("multer");
 const { dirname } = require("path");
 
+
 // server config
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -21,7 +22,19 @@ app.use(express.static("public"));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+//wit ai
+const serverToken=process.env.WITSERVERACCESSTOKEN
+
+
 app.post("/chatmessage", validationRules(), validate, (req, res, next) => {
   console.log(req.body);
   res.status(200).send({ response: "thank you for your message ." });
+
+  const encodedChat = encodeURIComponent(req.body.message);
+    const uri = "https://api.wit.ai/message?v=20220622&q=" + encodedChat;
+    const auth = "Bearer " + serverToken;
+    const chat = fetch(uri, { headers: { Authorization: auth } })
+      .then((res) => res.json())
+      .then((res) => console.log(res))
+
 });
