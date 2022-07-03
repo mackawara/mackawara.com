10a9ebb686404bf72c30a26059df4ae84f87f7eb
mackawara <84657473+mackawara@users.noreply.github.com>
2022-07-03 19:37:28 +0200

    added wit ai

--- public/js/index.js
+++ public/js/index.js
@@ -20,10 +20,16 @@ window.addEventListener("DOMContentLoaded", async () => {
       redirect: "follow",
       body: JSON.stringify({ message: message }),
     };
-    const responseMsg = await (await fetch("/chatmessage", options)).json();
+    /* const responseMsg = await (await fetch("/chatmessage", options)).json();
     const response = document.getElementById("response");
     response.innerText = responseMsg.response;
-    console.log(responseMsg.response);
+    console.log(responseMsg.response); */
+
+    const encodedChat = encodeURIComponent(message);
+    const uri = "https://api.wit.ai/message?v=20220622&q=" + encodedChat;
+    const auth = "Bearer " + CLIENT_TOKEN;
+    const chat = fetch(uri, { headers: { Authorization: auth } })
+      .then((res) => res.json())
+      .then((res) => console.log(res.));
   });
-  
 });
