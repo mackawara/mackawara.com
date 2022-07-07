f39c039aabbce0eb1345949ae072b147ccc66c73
mackawara <mkawara@outlook.com>
2022-07-07 20:34:57 +0200

    feat: added objects showing personal details

---
+++ assets/chatbotAssets.js
@@ -0,0 +1,7 @@
+const intentsList={
+    intents:[
+        "identity","location","pricing","languages",
+        "frameworks","jobOpportunities","technicalSkills","otherSkills","workExperience","projects", 
+    ]
+}
+module.exports=intentsList
---
+++ assets/mackawara.js
@@ -0,0 +1,8 @@
+const macKawara={
+    name: "Macdonald Kawara",
+    languages: ["Javascript","HTML", "CSS"],
+    frameworks:["ReactJS","NodeJs"],
+    keyLibraries: ["Express","bcrypt"],
+    jobOpportunities:["remote", "onSite", "frontEnd","backEnd","consultancy"]
+}
+module.exports=macKawara
