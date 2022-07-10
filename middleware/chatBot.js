368d6965c8ce2fd4327a29dcdfc8523f99505838
mackawara <mkawara@outlook.com>
2022-07-10 12:47:06 +0200

    chenged intent handling logic

--- middleware/chatBot.js
+++ middleware/chatBot.js
@@ -1,18 +1,30 @@
 //const intentsArray = require("../chatbotAssetsassets");
 const macKawara = require("../assets/mackawara");
-const chatBot = (intents, entity, confidence) => {
+const chatBot = (intents, entities, confidence) => {
+  let entityArray = [];
+  for (const any of Object.keys(entities)) {
+    entityArray.push(entities[any][0].name);
+  }
+  console.log(entityArray);
+
   for (const any of Object.keys(macKawara)) {
     const keysRegex = new RegExp(any, "i");
-    const values=macKawara[any]
-    const valuesRegex=new RegExp(values,"i")
+    const values = macKawara[any];
+    const valuesRegex = new RegExp(values, "i");
     /* if(keysRegex.test(intents)){
 } */
+    entityArray.forEach((entity) => {
+      if (keysRegex.test(entity) || valuesRegex.test(entity)) {
+        console.log( `These are my ${any}: ${macKawara[any]}`);
+        return
+      }
+    });
     intents.forEach((intent) => {
-      if (keysRegex.test(intent.name)) {
-        if(valuesRegex.test())
-        console.log(`These are my ${any}: ${macKawara[any]}`);
-      } else console.log("no match was found");
+      if (keysRegex.test(intent.name) || valuesRegex.test(intent.name)) {
+        console.log( `These are my2 ${any}: ${macKawara[any]}`);
+      }
     });
   }
 };
+
 module.exports = chatBot;
