e6ab5e0749fe30d7c3fc809c2b2a6c8a45bdebaf
mackawara <mkawara@outlook.com>
2022-07-10 15:56:47 +0200

    feat: created functionto handle intent entity

--- middleware/chatBot.js
+++ middleware/chatBot.js
@@ -1,19 +1,28 @@
 //const intentsArray = require("../chatbotAssetsassets");
 const macKawara = require("../assets/mackawara");
 const chatBot = (intents, entities, confidence) => {
-  const reqProps = {};
+  const entity = {};
   let entityArray = [];
+  let entityName, entityRole, entityValue, intent;
   for (const any of Object.keys(entities)) {
     //extract the name role and value from entity object
-    reqProps.entity = {};
-    reqProps.entity.name = entities[any][0].name;
-    reqProps.entity.role = entities[any][0].role;
-    reqProps.entity.value = entities[any][0].value;
+    // reqProps.entity = {};
+    entityName = entities[any][0].name;
+    entityRole = entities[any][0].role;
+    entityValue = entities[any][0].value;
   }
+  entity.entityName = entityName;
+  entity.entityRole = entityRole;
+  entity.entityValue = entityValue;
 
-  intents.forEach((intent) => {
-    reqProps.intent = intent.name;
+  intents.forEach((intentElement) => {
+    intent = intentElement.name;
   });
-  console.log(reqProps);
+  async function sendRes(intent, entity) {
+    console.log(intent);
+    console.log(entity.entityName, entity.entityRole, entity.entityValue);
+  }
+  sendRes(intent, entity);
+  // console.log(reqProps);
 };
 module.exports = chatBot;
