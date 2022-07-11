21173c077a961606d98c59d97947dd549352aebd
mackawara <mkawara@outlook.com>
2022-07-11 07:13:23 +0200

    added stengths intent

--- middleware/chatBot.js
+++ middleware/chatBot.js
@@ -20,12 +20,32 @@ const chatBot = (intents, entities, confidence) => {
     intent = intentElement.name;
   });
   async function sendRes(intent, entity) {
+    const role = entity.entityRole,
+      value = entity.entityValue,
+      entityName = entity.entityName;
+    const leadIam = `I am`;
+    const hChU = `How can I help you`;
+    console.log(role, value, entityName);
     console.log("hi how are you");
     if (intent === "greeting") {
       return "Hi , I am good thanks, how can I help you";
-    }
+    } else if (intent === "pricing") {
+      return "Thank you, for your enquiry, We can get back to you with a quote once the scope of work is fully established . Please leave your details so that my human can get back to you";
+    } else if (intent === "projects") {
+      if (value === "current projects")
+        return `I am currently working on these projects ${macKawara.projects.inProgress} `;
+      else if (value == "completed projects") {
+        `Checkout these completed  projects from my repo ${macKawara.projects.completed} `;
+      } else {
+        `I have a few games that I have built are ${macKawara.projects.games}`;
+      }
+    } else if (intent === "identity") {
+      return `Hi, I am ${macKawara.identity.name} from ${macKawara.identity.location} .${hChU}`;
+    } else if (intent === "strengths") {
+      return `My strengths are ${macKawara.ability}`;
+    } else if (intent === "job_opportunities") `${macKawara.jobOpportunities}`;
+    // console.log(reqProps);
   }
   return sendRes(intent, entity);
-  // console.log(reqProps);
 };
 module.exports = chatBot;
